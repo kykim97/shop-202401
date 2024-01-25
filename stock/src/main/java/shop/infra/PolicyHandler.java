a629dca0993677d99bc2e217fedda12aaa0225b2
kykim97 <113568664+kykim97@users.noreply.github.com>
2024-01-25 16:43:48 +0900

    commit

--- stock/src/main/java/shop/infra/PolicyHandler.java
+++ stock/src/main/java/shop/infra/PolicyHandler.java
@@ -2,8 +2,7 @@ package shop.infra;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.naming.NameParser;
-import javax.naming.NameParser;
+import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
